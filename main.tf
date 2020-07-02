35fe26acff287e2bc7bdae472562ee476c2e7ae6
Colin Simmons <colin.simmons@engineerbetter.com>
2020-07-02 10:09:37 +0100

    update to terrafor 0.12

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "website" {
 }
 
 resource "google_storage_bucket_acl" "website-acl" {
-  bucket = "${google_storage_bucket.website.name}"
+  bucket = google_storage_bucket.website.name
 
   predefined_acl = "publicRead"
 }
@@ -18,38 +18,38 @@ resource "google_storage_bucket_iam_member" "public-read" {
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 
-  depends_on = ["google_storage_bucket.website"]
+  depends_on = [google_storage_bucket.website]
 }
 
 resource "google_storage_bucket_object" "site" {
   name   = "index.html"
   source = "site/index.html"
-  bucket = "${google_storage_bucket.website.name}"
+  bucket = google_storage_bucket.website.name
 }
 
 resource "google_storage_bucket_object" "custom" {
   name         = "css/custom.css"
   source       = "site/css/custom.css"
   content_type = "text/css"
-  bucket       = "${google_storage_bucket.website.name}"
+  bucket       = google_storage_bucket.website.name
 }
 
 resource "google_storage_bucket_object" "normalize" {
   name         = "css/normalize.css"
   source       = "site/css/normalize.css"
   content_type = "text/css"
-  bucket       = "${google_storage_bucket.website.name}"
+  bucket       = google_storage_bucket.website.name
 }
 
 resource "google_storage_bucket_object" "skeleton" {
   name         = "css/skeleton.css"
   source       = "site/css/skeleton.css"
   content_type = "text/css"
-  bucket       = "${google_storage_bucket.website.name}"
+  bucket       = google_storage_bucket.website.name
 }
 
 resource "google_storage_bucket_object" "favicon" {
   name   = "images/favicon.png"
   source = "site/images/favicon.png"
-  bucket = "${google_storage_bucket.website.name}"
+  bucket = google_storage_bucket.website.name
 }
